4744be684c05dd288663abc797b290bd0b50fdb9
cxc_pc <965571656@qqcom>
2019-03-09 15:43:49 +0800

    todo

--- app/src/main/java/com/example/frameworkapp/global/App.java
+++ app/src/main/java/com/example/frameworkapp/global/App.java
@@ -56,6 +56,7 @@ public class App extends Application {
 
             @Override
             public void onCoreInitFinished() {
+                //todo
             }
         };
         //x5内核初始化接口
